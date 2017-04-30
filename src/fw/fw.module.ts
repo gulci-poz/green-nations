f810f34809e136494194ae4c1f9fdb7cb82d1e7a
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2017-04-30 05:36:36 +0200

    2017-04-30 screen resize service and responsive title bar

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -6,6 +6,7 @@ import {TitleBarComponent} from './title-bar/title-bar.component';
 import {FrameworkConfigService} from './services/framework-config.service';
 import {TopBarComponent} from './top-bar/top-bar.component';
 import {StatusBarComponent} from './status-bar/status-bar.component';
+import {ScreenService} from './services/screen.service';
 
 @NgModule({
   imports: [
@@ -18,7 +19,10 @@ import {StatusBarComponent} from './status-bar/status-bar.component';
     TopBarComponent,
     StatusBarComponent
   ],
-  providers: [FrameworkConfigService],
+  providers: [
+    FrameworkConfigService,
+    ScreenService
+  ],
   exports: [
     FrameworkBodyComponent
   ]
